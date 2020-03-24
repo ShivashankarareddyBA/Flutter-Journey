9e804288e36783b3995d61e0f0ffd8a0cbd43409
Shiva Shankar <ssr2tech@gmail.com>
2020-03-24 15:23:46 +0530

    complete Micard

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,76 +12,82 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Column(children: <Widget>[
-            Center(
-              child: CircleAvatar(
-                radius: 50.0,
-                backgroundColor: Colors.blue,
-                backgroundImage: AssetImage('Images/Logo.jpg'),
-              ),
-            ),
-            Text(
-              'Shiva',
-              style: TextStyle(
-                fontFamily: 'Pacifico',
-                fontSize: 40.0,
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Text(
-              'Android Developer',
-              style: TextStyle(
-                fontFamily: 'Source Sans pro',
-                color: Colors.teal.shade100,
-                fontSize: 20.0,
-                letterSpacing: 2.5,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Container(
-              color: Colors.white,
-              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
-              child: Row(
-                children: <Widget>[
-                  Icon(
-                    Icons.phone,
-                    color: Colors.pink,
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Center(
+                  child: CircleAvatar(
+                    radius: 50.0,
+                    backgroundColor: Colors.blue,
+                    backgroundImage: AssetImage('Images/Logo.jpg'),
                   ),
-                  SizedBox(
-                    width: 10.0,
+                ),
+                Text(
+                  'Shiva',
+                  style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    fontSize: 40.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
                   ),
-                  Text('+91 991 838 182',
-                      style: TextStyle(
-                        color: Colors.teal.shade900,
-                        fontFamily: 'Source Sans Pro',
-                        fontSize: 20.0,
-                      ))
-                ],
-              ),
-            ),
-            Container(
-                color: Colors.white,
-                padding: EdgeInsets.all(10.01),
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
-                child: Row(
-                  children: <Widget>[
-                    Icon(
-                      Icons.email,
-                      color: Colors.teal,
+                ),
+                Text(
+                  'Android Developer',
+                  style: TextStyle(
+                    fontFamily: 'Source Sans pro',
+                    color: Colors.teal.shade100,
+                    fontSize: 20.0,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                  width: 150.0,
+                  child: Divider(
+                    color: Colors.teal.shade100,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Card(
+                    margin:
+                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.phone,
+                        color: Colors.pink,
+                      ),
+                      title: Text('+91 991 838 182',
+                          style: TextStyle(
+                            color: Colors.teal.shade900,
+                            fontFamily: 'Source Sans Pro',
+                            fontSize: 20.0,
+                          )),
                     ),
-                    SizedBox(
-                      width:10.0,
-                    )
-                    Text('ssr2tech@gmail.com',
-                        style: TextStyle(
-                          fontSize: 20.0,
-                          color: Colors.teal.shade900,
-                          fontFamily: 'Source Sans Pro',
-                        )),
-                  ],
-                ))
-          ]),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Card(
+                      margin: EdgeInsets.symmetric(
+                          vertical: 10.0, horizontal: 10.0),
+                      child: ListTile(
+                        leading: Icon(
+                          Icons.email,
+                          color: Colors.teal,
+                        ),
+                        title: Text(
+                          'ssr2tech@gmail.com',
+                          style: TextStyle(
+                            fontSize: 20.0,
+                            color: Colors.teal.shade900,
+                            fontFamily: 'Source Sans Pro',
+                          ),
+                        ),
+                      )),
+                ),
+              ]),
         ),
       ),
     );
